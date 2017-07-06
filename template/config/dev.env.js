fcdfdc7d37ebb228d34fd4a892e61ede68f35618
lokialone <648875862@qq.com>
2017-07-06 11:21:08 +0800

    update

--- template/config/dev.env.js
+++ template/config/dev.env.js
@@ -4,7 +4,7 @@ var projectConfig = require('./project.config')
 
 var devConf = merge(commonConf, {
     env: 'dev',
-    assetsPublicPath: '//f2e.souche.com/projects/{{ department }}/{{ name }}/',
+    assetsPublicPath: '{{name}}',
     // globalConfig 中的 key 需要为 JSON 字符串
     globalConfig: {
         NODE_ENV: JSON.stringify("development")
